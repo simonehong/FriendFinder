db61d933a2209ee2e61af805bb672b6c14b7d701
Simon Hong <simon_hong1@yahoo.com>
2019-01-27 16:13:27 -0700

    updated code to deploy to heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var api = require('./app/routing/apiRoutes');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
